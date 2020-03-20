7cf7a740a1fbbec4365003869f5730487f1f8e76
James Clare <bluecombats@users.noreply.github.com>
2020-03-20 17:30:13 +0000

    First draft

---
+++ Timesheet.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name          TimeSheet reminders
+// @namespace     https://github.com/bluecombats/MediaCom/
+// @description	  Sends notifications from the timesheet website at set time to remind to enter in work.
+// @icon      		https://pub-resource-viewport.mediaocean.com/viewport/branding/rodick/2020.1.2/images/favicon.ico
+// @grant			none
+// @include       https://groupmuk-aura.mediaocean.com/*
+// @include       http*groupmuk-aura.mediaocean.com/*
+// @version        0.2
+// ==/UserScript==
+
+Notification.requestPermission().then(function(result) {
+	if (result === 'denied') {
+		console.log('Permission wasn\'t granted. Allow a retry.');
+		return;
+	}
+	if (result === 'default') {
+		console.log('The permission request was dismissed.');
+		return;
+	}
+  //set reminders times here
+  var reminderTimes=["17:20","15:50"];
+	MyVar=setInterval(function(){
+    //get current time
+    var d,h,m,s;
+    d=new Date();
+    h=d.getHours();
+    m=d.getMinutes();
+    s=d.getSeconds();
+    var i=0;
+    while(i<reminderTimes.Length){
+      var sh,sm;
+      sh=reminderTimes[i].substring(0,reminderTimes[i].indexOf(":")+1);
+      sh=Number(sh);
+      sm=reminderTimes[i].substring(reminderTimes[i].indexOf(":")+1,reminderTimes[i].Length);
+      sm=Number(sm);
+      if(sh == h && sm == m){
+        var notification=new Notification("Aura Timesheet",{
+          renotify:true
+          ,body="https://groupmuk-aura.mediaocean.com/"
+          ,icon="https://pub-resource-viewport.mediaocean.com/viewport/branding/rodick/2020.1.2/images/favicon.ico"
+        }
+      }
+      i+=1;
+    }
+	},3000);
+});
+//console.log("end of loop");
